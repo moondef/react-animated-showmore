fb2644a8d51cf8db2e8bbb34d99020b59feff2d4
moondef <stpnsamko@gmail.com>
2020-05-19 16:09:40 +0300

    Fix shadow transparance (doesn't work on IOS)

--- src/components/AnimatedShowMore/ShadowOverlay/index.js
+++ src/components/AnimatedShowMore/ShadowOverlay/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
-
-export const ShadowOverlay = ({ color = '#fff' }) => (
+export const ShadowOverlay = ({ color = "#fff" }) => (
   <div
     className="AnimatedShowMore__ShadowOverlay"
     style={{
-      backgroundImage: `linear-gradient(to bottom, transparent, ${color})`,
-      position: 'absolute',
+      backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0), ${color})`,
+      position: "absolute",
       bottom: 0,
       left: 0,
-      width: '100%',
-      height: 60
+      width: "100%",
+      height: 60,
     }}
   />
 );
